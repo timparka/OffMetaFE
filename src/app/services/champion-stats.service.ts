1447edfb0d626bf356e4b89305b1697ccf52eb2f
Tim Park <pimtarka@gmail.com>
2023-09-14 21:24:30 -0400

    added new endpoints to reach ec2 instance

--- src/app/services/champion-stats.service.ts
+++ src/app/services/champion-stats.service.ts
@@ -8,7 +8,7 @@ import { tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ChampionStatsService {
-  private apiUrl = 'http://localhost:8080/api/participant';
+  private apiUrl = 'http://3.88.117.135:8080/api/participant';
   private championData?: OffMetaDTO;
 
   constructor(private http: HttpClient) { }
